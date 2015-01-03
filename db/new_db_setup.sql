ccc3f0ad87c898f170e8b639ed562245a54cba7d
phazor <tristan.r.read@gmail.com>
2015-01-03 00:27:11 +0000

    db now uses separate movies and ratings tables with foreign key constraints

--- db/new_db_setup.sql
+++ db/new_db_setup.sql
@@ -1,8 +1,27 @@
+DROP TABLE ratings;
 DROP TABLE movies;
 
-CREATE TABLE movies (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, rating FLOAT, PRIMARY KEY (id));
+CREATE TABLE movies (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(50) NOT NULL, PRIMARY KEY (id));
 
-INSERT INTO movies (name, rating)
-VALUES ('Batman Begins', 5), ('The Grand Budapest Hotel', NULL);
+CREATE TABLE ratings (movie_id INT NOT NULL, rating FLOAT NOT NULL, username VARCHAR(50), FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE ON UPDATE CASCADE);
 
-SELECT * from movies;
+INSERT INTO movies (name)
+VALUES ('Batman Begins'), ('The Grand Budapest Hotel');
+
+-- INSERT INTO ratings (movie_id, rating, username)
+-- VALUES (1, 5, 't_rex'), (1, 4, 'ali_cat');
+
+INSERT INTO ratings (movie_id, rating, username)
+SELECT id, 5, 't_rex'
+FROM movies
+WHERE name = 'Batman Begins';
+
+INSERT INTO ratings (movie_id, rating, username)
+SELECT id, 4, 'ali_cat'
+FROM movies
+WHERE name = 'Batman Begins';
+
+-- SELECT * FROM movies LEFT JOIN ratings ON (ratings.movie_id = movies.id);
+
+SELECT * FROM movies;
+SELECT * FROM ratings;
